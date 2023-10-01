065e75e8870424df702ba314b564cdcc9fa71ad3
mayaj0yce <m.j.thayne@gmail.com>
2023-09-30 23:41:17 -0600

    more bones for models, meat on prior commit

--- models/Comments.js
+++ models/Comments.js
@@ -1,2 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+
+Comment.init(
+{
+    title: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user: {
+        // HOW
+    },
+    post: {
+        // also HOW
+    }
+}
+)
+
+module.exports = Comment;
